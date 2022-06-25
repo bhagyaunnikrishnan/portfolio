ab85b7665d75989a9b00ebae6c098d66c1552ede
akashrchandran <chandranrakash@gmail.com>
2022-06-25 14:14:29 +0530

    made toggle button responsive

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
-$(document).ready(function){
-    $('#menu').click(function(){
-        $(this).toggleClass('fa-times');
-        $('header').toggleClass('toggle');
+$( document ).ready(function() {
+  $('#menu').click(function(){
+    $(this).toggleClass('fa-times');
+    $('header').toggleClass('toggle');
+  })
 });
 
 $(window).on('scroll load',function(){
